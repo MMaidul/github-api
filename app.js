6caafbd10720b7b9e2d6aa84d98f9e48bfc8de0f
MMaidul <maidulmuqtadir360@gmail.com>
2022-03-09 19:09:37 +0600

    dynamically card added

--- app.js
+++ app.js
@@ -23,4 +23,28 @@ const searchProfile = ()=> {
 
 const displayProfile = (data)=> {
     console.log(data);
+    const cardParent = document.getElementById('card-parent');
+   const div = document.createElement('div');
+   div.classList.add('card');
+   div.innerHTML = `
+   <div class="row g-0">
+   <div class="col-md-4">
+     <img 
+     src="..." 
+     class="img-fluid rounded-start" 
+     alt="..."
+     />
+   </div>
+   <div class="col-md-8">
+     <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <p class="card-text">
+       This is a wider card with supporting text below as a natural 
+       lead-in to additional content. This content is a little bit longer.
+       </p>
+     </div>
+   </div>
+  </div>
+   `;
+   cardParent.appendChild(div);
 }
